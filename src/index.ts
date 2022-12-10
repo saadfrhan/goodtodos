b268461292cd43f3505139f9bf7180801d88adc8
SaadFarhanIdress <saadfarhan347@gmail.com>
2022-12-10 04:00:16 +0000

    feat: tweaks

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import main from './main.js';
 import welcome from './utils/welcome.js';
 
-await welcome();
-await main().starterQuestion();
+(async () => {
+  await welcome();
+  await main().starterQuestion();
+})();
